0cf325781a152e4fdcde0db3bffdcae0e0c0ba99
jimena <jimenahernando@hotmail.com>
2021-08-17 13:12:16 +0200

    Proyecto aficiones. Estructural Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { ListaAficionesComponent } from './components/lista-aficiones/lista-aficiones.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ListaAficionesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/lista-aficiones/lista-aficiones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-aficiones',
+  templateUrl: './lista-aficiones.component.html',
+  styleUrls: ['./lista-aficiones.component.css']
+})
+export class ListaAficionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
